c5555815a06e8715bdd1baf3adff1aa6d0c69a70
AlexTischer <alessandro1.tischer@mail.polimi.it>
2025-04-24 15:18:22 +0200

    committed correct python scripts

--- trains-api-tools/maps_asker.py
+++ trains-api-tools/maps_asker.py
@@ -3,23 +3,50 @@
 import requests
 import json
 import os
+import time
+from datetime import datetime
 
-# Sostituisci con la tua API key di Google Maps
-API_KEY = 'AIzaSyAfKJSrrRqlMH63KucLkKf6huh2JMfujZU'
+
 # Esempio di endpoint: Geocoding API
 endpoint = 'https://maps.googleapis.com/maps/api/directions/json?'
 
+def get_user_inputs():
+    origin = input("Inserisci la stazione di partenza: ").strip()
+    destination = input("Inserisci la stazione di arrivo: ").strip()
+    arrival_time_str = input("Inserisci l'orario di arrivo (formato: YYYY-MM-DD HH:MM): ").strip()
+    api_key = input("Inserisci la tua API key di Google Maps: ").strip()
+    if not api_key:
+        print("API key non valida.")
+        exit(1)
+    if not origin or not destination:
+        print("Stazione di partenza o arrivo non validi.")
+        exit(1)
+    if not arrival_time_str:
+        print("Orario di arrivo non valido.")
+        exit(1)
+    try:
+        dt = datetime.strptime(arrival_time_str, "%Y-%m-%d %H:%M")
+        arrival_time = int(time.mktime(dt.timetuple()))
+    except ValueError:
+        print("Formato orario non valido. Usa YYYY-MM-DD HH:MM.")
+        exit(1)
+    return origin, destination, arrival_time, api_key
+
 # Parametri della richiesta (esempio: indirizzo da geocodificare)
 params = {
-    "origin": "Bergamo FS",
-    "destination": "busto arsizio FS",
     "mode": "transit",
     "transit_mode": "train",
-    "arrival_time": 1744912744,
     "alternatives": "true",
     "region": "it",
-    "key": API_KEY,
 }
+
+
+origin, destination, arrival_time, apy_key = get_user_inputs()
+params["origin"] = origin
+params["destination"] = destination
+params["arrival_time"] = arrival_time
+params["key"] = apy_key
+
 
 def main():
     response = requests.get(endpoint, params=params)
