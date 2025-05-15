e9dce0c07531046b8ec9e1fb33cfa87bfcc21367
Nume99 <emanuele.musto@mail.polimi.it>
2025-05-15 11:33:10 +0200

    Fixed facebook login

--- train_tribe/android/app/src/main/kotlin/com/example/train_tribe/MainActivity.kt
+++ train_tribe/android/app/src/main/kotlin/com/example/train_tribe/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.train_tribe
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity()
