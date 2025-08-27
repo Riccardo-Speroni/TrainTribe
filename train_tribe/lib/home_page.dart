26d27a19795789316c2b6e2747ccce3dd962f0ef
Riccardo Speroni <riccardo.speroni@mail.polimi.it>
2025-08-27 20:31:13 +0200

    Added a pool of questions to show in the home page

--- train_tribe/lib/home_page.dart
+++ train_tribe/lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +11,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   bool isSwitch = false;
+
+  final List<String> moodQuestionsKeys = [
+    'mood_question_1',
+    'mood_question_2',
+    'mood_question_3',
+    'mood_question_4',
+    'mood_question_5',
+  ];
+
+  late String selectedMoodQuestionKey;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedMoodQuestionKey = moodQuestionsKeys[Random().nextInt(moodQuestionsKeys.length)];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +42,7 @@ class _HomePageState extends State<HomePage> {
             width: 200,
           ),
           const Spacer(flex: 2),
-          Text(localizations.translate('mood_question'), style: const TextStyle(fontSize: 24)), 
+          Text(localizations.translate(selectedMoodQuestionKey), style: const TextStyle(fontSize: 24)), 
           Switch(
             value: isSwitch,
             onChanged: (bool newBool) {
